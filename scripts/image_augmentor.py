580bd303c8c1b17f5632aebd1552178fee091b38
Rafael Santos <rafael.santos.bra@gmail.com>
2020-05-28 12:13:00 -0400

    Refactor rotation augmentation

--- scripts/image_augmentor.py
+++ scripts/image_augmentor.py
@@ -6,15 +6,50 @@ from PIL import Image
 import numpy as np
 import pathlib
 
+
 def main(args=None):
-    source_file_paths = obtain_source_file_paths()
-    images = read_images(source_file_paths)
-    rotate = iaa.Affine(rotate=(-25, 25))
-    image_aug = rotate(image=images[0])
-    target_image_name = source_file_paths[0].name
-    image_obj = Image.fromarray(image_aug)
-    target_image_path = pathlib.Path.cwd().joinpath('images').joinpath('target').joinpath(target_image_name)
-    image_obj.save(target_image_path)
+    source_image_paths = build_source_file_paths()
+    images = read_images(source_image_paths)
+    source_images_by_name = build_images_by_name(images, source_image_paths)
+    augmented_images_by_name = augment(source_images_by_name)
+    save_images(augmented_images_by_name)
+
+
+def save_images(augmented_images_by_name):
+    for image_name in augmented_images_by_name:
+        image = augmented_images_by_name[image_name]
+        image_obj = Image.fromarray(image)
+        target_image_path = pathlib.Path.cwd().joinpath('images').joinpath('target').joinpath(image_name)
+        image_obj.save(target_image_path)
+
+
+def augment(images_by_name):
+    augmented_images_by_name = {}
+    for image_name in images_by_name:
+        image = images_by_name[image_name]
+        image_aug = augment_rotation(image_name, image)
+        for name in image_aug:
+            augmented_images_by_name[name] = image_aug[name]
+    return augmented_images_by_name
+
+
+def augment_rotation(image_name, image):
+    result = {}
+    images = [image, image, image, image, image]
+    rotate = iaa.Affine(rotate=(-30, 30))
+    augmented_images = rotate(images=images)
+    for i in range(len(augmented_images)):
+        augmented_image_name = '{}_rotation_{}.jpg'.format(image_name, i)
+        result[augmented_image_name] = augmented_images[i]
+    return result
+
+
+def build_images_by_name(images, source_image_paths):
+    images_by_target_path = {}
+    for i in range(len(source_image_paths)):
+        images_by_target_path[source_image_paths[i].name.replace('.jpg', '')] = images[i]
+    return images_by_target_path
+
 
 def read_images(source_file_paths):
     images = []
@@ -22,7 +57,8 @@ def read_images(source_file_paths):
         images.append(imageio.imread(f))
     return images
 
-def obtain_source_file_paths():
+
+def build_source_file_paths():
     source_dir = pathlib.Path.cwd().joinpath('images').joinpath('source')
     source_files = [p for p in source_dir.glob('*') if p.is_file()]
     for f in source_files:
