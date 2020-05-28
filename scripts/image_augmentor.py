46245bdf2da57a0ea0db0a54ad9efa1a1c023c4b
Rafael Santos <rafael.santos.bra@gmail.com>
2020-05-28 17:34:11 -0400

    Added hard coded Labeled boxes

--- scripts/image_augmentor.py
+++ scripts/image_augmentor.py
@@ -7,15 +7,16 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 import pathlib
+from pathlib import Path
 
-from scripts.structs.image_struct import ImageStruct
+from scripts.structs.image_struct import ImageStruct, LabeledBox
 
 
 def main(args=None):
-    source_image_paths = build_source_file_paths()
-    images = read_images(source_image_paths)
+    source_image_paths: List[Path] = build_source_file_paths()
+    image_structs: List[ImageStruct] = build_image_structs(source_image_paths)
     # TODO: List of BoundingBoxes
-    image_structs = build_image_structs(source_image_paths, images)
+    # image_structs = build_image_structs(source_image_paths, images, labeled_boxes)
     image_structs = augment(image_structs)
     save_images(image_structs)
 
@@ -27,7 +28,7 @@ def save_images(image_structs: List[ImageStruct]):
         image_pil.save('{}.jpg'.format(str(target_image_path)))
 
 
-def augment(image_structs: List[ImageStruct]):
+def augment(image_structs: List[ImageStruct]) -> List[ImageStruct]:
     augmentations: List[ImageStruct] = []
     # Augmentations
     for image_struct in image_structs:
@@ -65,21 +66,27 @@ def augment_noise(image_struct: ImageStruct):
     return result
 
 
-def build_image_structs(image_paths, images):
-    result: List[ImageStruct] = []
-    for i in range(len(image_paths)):
-        image_name = image_paths[i].name.replace('.jpg', '')
-        result.append(ImageStruct(image_name, images[i], None))
-    return result
-
-
 def read_images(source_file_paths):
     return [imageio.imread(f) for f in source_file_paths]
 
 
-def build_source_file_paths():
+def build_source_file_paths() -> Path:
     source_dir = pathlib.Path.cwd().joinpath('images').joinpath('source')
     return [p for p in source_dir.glob('*.jpg') if p.is_file()]
+
+
+def build_image_structs(image_paths: List[Path]) -> List[ImageStruct]:
+    result: List[ImageStruct] = []
+    for image_path in image_paths:
+        image = imageio.imread(image_path)
+        image_name = image_path.name.replace('.jpg', '')
+
+        image_boxes: List[LabeledBox] = []
+        label_path = image_path.parent.joinpath(image_name + '.xml')
+        image_boxes.append(LabeledBox(10, 20, 100, 200))
+        result.append(ImageStruct(image_name, image, image_boxes))
+    return result
+
 
 if __name__ == '__main__':
     main()
