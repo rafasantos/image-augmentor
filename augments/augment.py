f1925e98a6d8d3c036ca68abf733c947ff82d980
Rafael Santos <rafael.santos.bra@gmail.com>
2020-05-28 23:17:11 -0400

    Added for augmentation

--- augments/augment.py
+++ augments/augment.py
@@ -1,25 +1,22 @@
+import random
 from typing import List
 
 from imgaug.augmenters import Affine, AdditiveGaussianNoise
-from imgaug.augmenters.imgcorruptlike import ElasticTransform
+from imgaug.augmenters.imgcorruptlike import ElasticTransform, Fog
 
 from builders.meta_image import to_bounding_boxes_on_image, to_labeled_boxes
 from struts.meta_image import MetaImage
 
 
-def augment_elastic(m_image: MetaImage) -> MetaImage:
-    elastic_transform = ElasticTransform(severity=[1, 1])
-    augmented_image = elastic_transform(image=m_image.data)
-    return MetaImage(m_image.name, augmented_image, m_image.labeled_boxes)
-
-
 def augment(m_images: List[MetaImage]) -> List[MetaImage]:
     augmentations: List[MetaImage] = []
-    # augmentations: List[MetaImage] = []
-    # # Augmentations
+    # Augmentations
     for m_image in m_images:
-        elastic_image = augment_elastic(m_image)
+        elastic_image = augment_elastic_transformation(m_image)
         augmentations.append(elastic_image)
+
+        fogged_image = augment_fog(m_image)
+        augmentations.append(fogged_image)
 
         noise_images_structs = augment_noise(m_image)
         augmentations.extend(noise_images_structs)
@@ -32,6 +29,21 @@ def augment(m_images: List[MetaImage]) -> List[MetaImage]:
 
     return result
     return augmentations
+
+
+def augment_elastic_transformation(m_image: MetaImage) -> MetaImage:
+    augmentation = ElasticTransform(severity=[1, 1])
+    augmented_image = augmentation(image=m_image.data)
+    image_name = '{}_elas_'.format(m_image.name)
+    return MetaImage(image_name, augmented_image, m_image.labeled_boxes)
+
+
+def augment_fog(m_image: MetaImage) -> MetaImage:
+    severity = random.randint(1, 3)
+    augmentation = Fog(severity=severity)
+    augmented_image = augmentation(image=m_image.data)
+    image_name = '{}_fog_'.format(m_image.name)
+    return MetaImage(image_name, augmented_image, m_image.labeled_boxes)
 
 
 def augment_noise(m_image: MetaImage):
@@ -47,12 +59,9 @@ def augment_noise(m_image: MetaImage):
 
 def augment_rotation(m_image: MetaImage):
     result: List[MetaImage] = []
-    images = [m_image.data, m_image.data, m_image.data, m_image.data]
-    boxes = [to_bounding_boxes_on_image(m_image),
-             to_bounding_boxes_on_image(m_image),
-             to_bounding_boxes_on_image(m_image),
-             to_bounding_boxes_on_image(m_image)]
     rotate = Affine(rotate=(-30, 30))
+    images = [m_image.data]
+    boxes = [to_bounding_boxes_on_image(m_image)]
     augmentations, augmented_boxes = rotate(images=images, bounding_boxes=boxes)
     for i in range(len(augmentations)):
         image_name = '{}_rot_{}'.format(m_image.name, i)
